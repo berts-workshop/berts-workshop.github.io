d346d875e34a1ca34ffde53b3809a7bd88a535af
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2025-12-06 19:53:10 +0000

    Deploying to gh-pages from @ berts-workshop/berts-workshop.github.io@c7b7463dbb6ecf922c7b71b5972a3ce7059d5690 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/call-for-papers/";
           },
-        },{id: "nav-speakers",
-          title: "Speakers",
-          description: "List of invited speakers and panel members.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/speakers/";
-          },
         },{id: "nav-organizers",
           title: "Organizers",
           description: "",
