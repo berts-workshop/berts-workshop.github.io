5c805e7d52ac2f6bd7785b384ee973f2efe5b97a
berts-workshop <berts2025.workshop@gmail.com>
2026-03-16 09:20:34 +0000

    Deploying to gh-pages from @ berts-workshop/berts-workshop.github.io@8230007e746f555da5ddc3a72753ded3d15228ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/call-for-papers/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "nav-speakers",
           title: "Speakers",
           description: "List of invited speakers and panel members.",
